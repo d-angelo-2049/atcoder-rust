05e9b7d6ea614582d356941d0d1c994fa80ce19d
D'Angelo <63720046+d-angelo-2049@users.noreply.github.com>
2023-11-02 00:15:27 +0900

    added (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// A-Circle
+// https://atcoder.jp/contests/abc145/tasks/abc145_a
+
 fn main() {
-    println!("Hello, world!");
+    proconio::input! {
+        r: i32,
+    }
+    println!("{}", r.pow(2));
 }
